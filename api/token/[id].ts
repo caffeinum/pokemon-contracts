4a899ce4c2a0d6e8a14cc958c4d89905989e29a6
Aleksey Bykhun <theisease@gmail.com>
2022-02-07 07:26:13 +0800

    add metadata dummy

---
+++ api/token/[id].ts
@@ -0,0 +1,27 @@
+import type { VercelApiHandler } from '@vercel/node';
+
+import { allowCors } from '../_cors';
+
+import metadata from '../../_json_metadata/pokemon_metadata.json';
+
+
+// /api/token/[id]?data=0xhasheddata
+const handler: VercelApiHandler = async (request, response) => {
+    const { id, data } = request.query;
+
+    // https://vercel.com/docs/serverless-functions/edge-caching#stale-while-revalidate
+    // Which tells our CDN: serve from cache, but update it, if requested after 2 seconds.
+    response.setHeader('Cache-Control', 's-maxage=2, stale-while-revalidate')
+
+    const tokenId = Number(id)
+
+    return response.status(200).json({
+        name: 'Pokemon DAO',
+        image: 'https://raw.githubusercontent.com/vercel/vercel/master/examples/api/public/pokemon.png',
+        token: metadata[tokenId],
+        attributes: [
+        ]
+    });
+};
+
+export default allowCors(handler);

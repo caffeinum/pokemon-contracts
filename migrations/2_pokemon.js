f5e315881e5733b829aa1806f62b309b3d4017af
Aleksey Bykhun <theisease@gmail.com>
2022-02-07 10:12:05 +0800

    fix nft address

--- migrations/2_pokemon.js
+++ migrations/2_pokemon.js
@@ -4,7 +4,7 @@ const ADMIN = '0xffE06cb4807917bd79382981f23d16A70C102c3B';
 
 module.exports = async function (deployer) {
   // https://polygonscan.com/address/0x8eb626335f2303adcd7c32c32ef4bd1e1b2957ec
-  const NFT_ADDRESS = '0x8eb626335f2303AdCd7C32C32ef4BD1E1b2957EC';
+  const NFT_ADDRESS = '0x082b3c4ee12471254c578dae7ad3a996c8840198';
 
   await deployer.deploy(PokemonExtension, NFT_ADDRESS);
 
